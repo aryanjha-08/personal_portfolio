b6b77fcebee9ed3e689eefc1e1e9cc1ab56208f3
Aryan jha <aryanjha101@gmail.com>
2023-09-08 00:34:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemnu= document.getElementById("#sidemenu");
+
+function openmenu(){
+    sidemnu.style.right="0";
+}
+function closemennu(){
+    sidemnu.style.right="-200px";
+}
